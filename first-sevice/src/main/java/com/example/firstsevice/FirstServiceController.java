1e88d90e7a9f9b16157dcde471cb8e2893fa85f9
audgks5551 <audgks5551@daum.net>
2022-03-13 22:15:37 +0900

    first-service 두개 사용하여 port번호 확인

--- first-sevice/src/main/java/com/example/firstsevice/FirstServiceController.java
+++ first-sevice/src/main/java/com/example/firstsevice/FirstServiceController.java
@@ -1,15 +1,23 @@
 package com.example.firstsevice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +31,11 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, There! This is a message from first Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format(
+                "Hi, There! This is a message from first Service on PORT %s",
+                env.getProperty("local.server.port")
+        );
     }
 }
